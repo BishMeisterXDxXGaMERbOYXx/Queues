a775a864d049aef1926827387b91272943e5465e
Bishman Karnan <K033888@eltham-college.org.uk>
2024-01-24 09:30:33 +0000

    good

--- CircularQueue.cs
+++ CircularQueue.cs
@@ -8,5 +8,70 @@ namespace Queues
 {
     internal class CircularQueue
     {
+        private string[] circularQueue;
+        private String[] deQueuedItems;
+        private int front, f;
+        private int rear;
+        private string dequeuedItem;
+
+        public CircularQueue(int s)
+        {
+            circularQueue = new string[s];
+            deQueuedItems = new string[s];
+            front = 0;
+            rear = -1;
+        }
+
+        public bool isFull()
+        {
+            if ((rear + 1) % circularQueue.Length == front)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool initialised()
+        {
+            return front == 0 && rear == -1;
+        }
+
+        public bool isEmpty()
+        {
+            return ((front + 1) % circularQueue.Length == 0 && rear == circularQueue.Length - 1) || ((front + 1) > rear && !initialised()) && !isFull() || initialised();
+        }
+        public void enQueue(string d)
+        {
+            if (initialised() || !isFull())
+            {
+                rear = (rear + 1) % circularQueue.Length;
+                circularQueue[rear] = d;
+            }
+            else
+            {
+                Console.WriteLine("Full queue, remove item first!");
+            }
+        }
+
+        public void deQueue()
+        {
+            if (isEmpty())
+            {
+                Console.WriteLine("Queue empty");
+            }
+            else
+            {
+                front = (front + 1) % circularQueue.Length;
+                dequeuedItem = circularQueue[front];
+                f += 1;
+                dequeuedItems[f] = dequeuedItems;
+            }
+
+        }
+
+
     }
 }
